c17199eb5a37d54fa598633cc7c8f0e36ff51cf7
Ubuntu 18.04 x64 <kirakis1@gmail.com>
2018-09-02 05:23:11 -0700

    init

---
+++ ts.py
@@ -0,0 +1,9 @@
+import platform
+import os
+
+dirPath = os.getcwd()
+print("Current directory is: " + dirPath)
+folderName = os.path.basename(dirPath)
+print ('DirectoryName is' + folderName)
+print(platform.python_version())
+print(1)
